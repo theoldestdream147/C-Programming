bd996cdb8df89ccfe716d816522ff26fed2b6ece
Aditya Raj Singh <the.oldest.dream.147@gmail.com>
2026-09-09 20:55:50 +0530

    loops

---
+++ 04-Loops/Do_while.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    int i= 0;
+    do{
+        if (i>=10){
+            printf("the vakkue of i is %d \n",i);
+            i++;
+        }else{
+            i++;
+        }
+    }while (i<=20);
+    return 0;
+    
+}
