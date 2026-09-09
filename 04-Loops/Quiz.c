0e0bbb8e16fe1a11abe3854d524c9ac39ef66d50
Aditya Raj Singh <the.oldest.dream.147@gmail.com>
2026-09-09 21:14:22 +0530

    loops

---
+++ 04-Loops/Quiz.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int n ;
+    printf("Enter a number:");
+    scanf("%d ",&n);
+    for (int i = n; i>=1;i--){
+        printf("%d\n",i);
+    }
+    return 0;
+}
