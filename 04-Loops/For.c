b55868567c7e02bbb5c0220d2dcb7f54c4592320
Aditya Raj Singh <the.oldest.dream.147@gmail.com>
2026-09-09 21:01:43 +0530

    loops

---
+++ 04-Loops/For.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    for(int i = 0; i<=20;i++){
+        if (i>=10){
+            printf("the value of i is %d \n",i);
+        }else{
+            i++;
+        }
+    
+    }
+return 0;
+}
